46f9614c7c45c3d3adb3f54bd561c5640cc65bf8
Mustafa <mm0118250@gmail.com>
2025-03-30 14:51:17 +0200

    solving the problem of the keyboard behavior and refactor some code

--- lib/screens/onboarding/onboarding_screen.dart
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -1,8 +1,10 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:rive/rive.dart' as rive;
 import 'package:rive_animation/screens/components/animated_btn.dart';
+import 'package:rive_animation/screens/components/custom_signin_dialog.dart';
 import 'package:rive_animation/screens/components/sign_in_form.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -13,18 +15,19 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+  bool isSignInDialogShow = false;
   late rive.RiveAnimationController _btnAnimation;
   @override
   void initState() {
     _btnAnimation = rive.OneShotAnimation('active', autoplay: false);
 
-  
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       body: Stack(
         children: [
           Positioned(
@@ -45,127 +48,70 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
               child: const SizedBox(),
             ),
           ),
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32),
-              child: Column(
-                children: [
-                  SizedBox(
-                    width: 260,
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 50),
-                        const Text(
-                          'Learn design & code',
-                          style: TextStyle(
-                            fontSize: 60,
-                            fontFamily: 'Poppins',
-                            height: 1.2,
+          AnimatedPositioned(
+            top: isSignInDialogShow ? -50 : 0,
+            duration: const Duration(milliseconds: 240),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: 260,
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 50),
+                          const Text(
+                            'Learn design & code',
+                            style: TextStyle(
+                              fontSize: 60,
+                              fontFamily: 'Poppins',
+                              height: 1.2,
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 16),
-                        const Text(
-                          'Don\'t skip design, learn design & code by building beautiful Mobile apps and developing it so you make your understand for the work will be more stronger and have a good experience in the tech.',
-                        ),
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * .25,
-                        ),
-                        AnimatedBtn(
-                          btnAnimation: _btnAnimation,
-                          press: () {
-                            _btnAnimation.isActive = true;
-                            showGeneralDialog(
-                              barrierDismissible: true,
-                              barrierLabel: "Sign In",
-                              context: context,
-                              pageBuilder: (context, _, _) {
-                                return Center(
-                                  child: Center(
-                                    child: Container(
-                                      height: 620,
-                                      padding: const EdgeInsets.symmetric(
-                                        horizontal: 20,
-                                      ),
-                                      margin: const EdgeInsets.symmetric(
-                                        horizontal: 16,
-                                      ),
-                                      decoration: BoxDecoration(
-                                        color: Colors.white.withOpacity(0.94),
-                                        borderRadius: const BorderRadius.all(
-                                          Radius.circular(16),
-                                        ),
-                                      ),
-                                      child: const Scaffold(
-                                        backgroundColor: Colors.transparent,
-                                        body: Column(
-                                          children: [
-                                            Text(
-                                              'Sign In',
-                                              style: TextStyle(
-                                                fontSize: 34,
-                                                fontWeight: FontWeight.bold,
-                                                fontFamily: 'Poppins',
-                                              ),
-                                            ),
-                                            Padding(
-                                              padding: EdgeInsets.symmetric(
-                                                horizontal: 16,
-                                              ),
-                                              child: Text(
-                                                'Access to 240+ hours of content. Learn design and code, by building real apps with flutter and swift',
-                                                textAlign: TextAlign.center,
-                                              ),
-                                            ),
-                                            SizedBox(height: 30),
-                                            SignInForm(),
-                                            Row(
-                                              children: [
-                                                Expanded(child: Divider()),
-                                                Padding(
-                                                  padding: EdgeInsets.symmetric(
-                                                    horizontal: 12,
-                                                  ),
-                                                  child: Text(
-                                                    'OR',
-                                                    style: TextStyle(
-                                                      color: Colors.black26,
-                                                    ),
-                                                  ),
-                                                ),
-                                                Expanded(child: Divider()),
-                                              ],
-                                            ),
-                                            Padding(
-                                              padding: EdgeInsets.symmetric(
-                                                vertical: 20,
-                                              ),
-                                              child: Text(
-                                                'Sign Up with Email, Apple or Google.',
-                                                style: TextStyle(
-                                                  color: Colors.black54
-                                                ),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              },
-                            );
-                          },
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 24),
-                          child: Text(
-                            'Purchase includes 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certifications.',
+                          const SizedBox(height: 16),
+                          const Text(
+                            'Don\'t skip design, learn design & code by building beautiful Mobile apps and developing it so you make your understand for the work will be more stronger and have a good experience in the tech.',
                           ),
-                        ),
-                      ],
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * .25,
+                          ),
+                          AnimatedBtn(
+                            btnAnimation: _btnAnimation,
+                            press: () {
+                              _btnAnimation.isActive = true;
+                              Future.delayed(
+                                const Duration(milliseconds: 800),
+                                () {
+                                  setState(() {
+                                    isSignInDialogShow = true;
+                                  });
+                                  customSignInDialog(
+                                    context,
+                                    onClose: (_) {
+                                      setState(() {
+                                        isSignInDialogShow = false;
+                                      });
+                                    },
+                                  );
+                                },
+                              );
+                            },
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 24),
+                            child: Text(
+                              'Purchase includes 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certifications.',
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
